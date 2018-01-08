1d2bc4b69d98f62873e9f3f0ff1909cec033b61e
Nick Franklin <n.o.franklin@gmail.com>
2018-01-07 22:52:14 -0500

    Basic VOTER pipeline complete.

--- VOTER/README.md
+++ VOTER/README.md
@@ -2,8 +2,14 @@
 
 This pipeline creates tables within the VOTER dataset.
 
-To run on Python 2, and assuming you have the Cloud Dataflow SDK already installed:
+To run on Cloud Dataflow, and assuming you have the Cloud Dataflow SDK already installed:
 
 ```
-python pipeline.py -runner DataflowRunner --temp_location gs://<temp bucket> --staging_location gs://<staging bucket>
+python pipeline.py -runner=DataflowRunner --temp_location=gs://<temp bucket> --staging_location=gs://<staging bucket> --max_num_workers=21 --requirements_file=requirements.txt --usps_key=<API key>
+```
+
+To test in local mode:
+
+```
+python pipeline.py -runner=DirectRunner --temp_location=gs://<temp bucket> --staging_location=gs://<staging bucket> --requirements_file=requirements.txt --usps_key=<API key>
 ```
--- VOTER/pipeline.py
+++ VOTER/pipeline.py
@@ -1,4 +1,12 @@
 """A pipeline to process New York State voter file and store in BigQuery.
+
+TODO:
+- Move GCS input file to command-line
+- Pick local/remote mode easier
+- Split VoterHistory, although unfortunatley this requires more involved schema definitions:
+    https://reformatcode.com/code/python/json-table-schema-to-bigquerytableschema-for-bigquerysink
+
+
 """
 
 from __future__ import absolute_import
@@ -7,87 +15,225 @@ import argparse
 import logging
 import csv
 
+from pyusps import address_information
+from datetime import date, datetime
+
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.options.pipeline_options import SetupOptions
 
 RAW_VF_SCHEMA = (
-     "LASTNAME:STRING,FIRSTNAME:STRING,MIDDLENAME:STRING,"
-     "NAMESUFFIX:STRING,RADDNUMBER:STRING,RHALFCODE:STRING,"
-     "RAPARTMENT:STRING,RPREDIRECTION:STRING,RSTREETNAME:STRING,"
-     "RPOSTDIRECTION:STRING,RCITY:STRING,RZIP5:STRING,RZIP4:STRING,"
-     "MAILADD1:STRING,MAILADD2:STRING,MAILADD3:STRING,MAILADD4:STRING,"
-     "DOB:STRING,GENDER:STRING,ENROLLMENT:STRING,OTHERPARTY:STRING,"
-     "COUNTYCODE:INTEGER,ED:INTEGER,LD:INTEGER,TOWNCITY:STRING,"
-     "WARD:STRING,CD:INTEGER,SD:INTEGER,AD:INTEGER,LASTVOTEDDATE:STRING,"
-     "PREVYEARVOTED:STRING,PREVCOUNTY:STRING,PREVADDRESS:STRING,"
-     "PREVNAME:STRING,COUNTYVRNUMBER:STRING,REGDATE:STRING,VRSOURCE:STRING,"
-     "IDREQUIRED:STRING,IDMET:STRING,STATUS:STRING,REASONCODE:STRING,"
-     "INACT_DATE:STRING,PURGE_DATE:STRING,SBOEID:STRING,"
-     "VoterHistory:STRING,Phone:STRING")
+    "LASTNAME:STRING,FIRSTNAME:STRING,MIDDLENAME:STRING,"
+    "NAMESUFFIX:STRING,RADDNUMBER:STRING,RHALFCODE:STRING,"
+    "RAPARTMENT:STRING,RPREDIRECTION:STRING,RSTREETNAME:STRING,"
+    "RPOSTDIRECTION:STRING,RCITY:STRING,RZIP5:STRING,RZIP4:STRING,"
+    "MAILADD1:STRING,MAILADD2:STRING,MAILADD3:STRING,MAILADD4:STRING,"
+    "DOB:STRING,GENDER:STRING,ENROLLMENT:STRING,OTHERPARTY:STRING,"
+    "COUNTYCODE:INTEGER,ED:INTEGER,LD:INTEGER,TOWNCITY:STRING,"
+    "WARD:STRING,CD:INTEGER,SD:INTEGER,AD:INTEGER,LASTVOTEDDATE:STRING,"
+    "PREVYEARVOTED:STRING,PREVCOUNTY:STRING,PREVADDRESS:STRING,"
+    "PREVNAME:STRING,COUNTYVRNUMBER:STRING,REGDATE:STRING,VRSOURCE:STRING,"
+    "IDREQUIRED:STRING,IDMET:STRING,STATUS:STRING,REASONCODE:STRING,"
+    "INACT_DATE:STRING,PURGE_DATE:STRING,SBOEID:STRING,"
+    "VoterHistory:STRING,Phone:STRING")
+
+FORMATTED_SCHEMA = (
+    "LastName:STRING,FirstName:STRING,MiddleName:STRING,NameSuffix:STRING,"
+    "Gender:STRING,Enrollment:STRING,OtherPartyEnrollment:STRING,"
+    "ElectionDistrict:STRING,LegislativeDistrict:INTEGER,TownCityCode:STRING,"
+    "Ward:STRING,CongressionalDistrict:INTEGER,SenateDistrict:INTEGER,"
+    "AssemblyDistrict:INTEGER,PreviousName:STRING,CountyVoterNumber:STRING,"
+    "VoterRegistrationSource:STRING,IDRequired:STRING,IDMet:STRING,"
+    "Status:STRING,StatusReasonCode:STRING,SBOEID:STRING,DateOfBirth:DATE,"
+    "LastVotedDate:DATE,InactivatedDate:DATE,PurgedDate:DATE,Age:INTEGER,"
+    "Address:STRING,StreetAddress:STRING,VoterHistory:STRING")
 
 class DictFromRawLine(beam.DoFn):
-    """Create Dict of strings from raw line of Voter File CSV."""
+    """Parse line in input CSV and generate row in VoterFileRaw table."""
 
     def process(self, element):
         try:
             for data in csv.reader([element]):
                 dct = {}
                 for i, field in enumerate(RAW_VF_SCHEMA.split(",")):
-                    name = field.split(":")[0]
+                    name, typ = field.split(":")
                     try:
-                        dct[name] = data[i].encode('utf-8').strip()
+                        if typ == "STRING":
+                            dct[name] = data[i].encode('utf-8').strip()
+                        elif typ == "INTEGER":
+                            dct[name] = int(data[i].encode('utf-8').strip())
                     except Exception:
                         dct[name] = None
                 yield dct
         except Exception:
-            pass
+            logging.error("Error in csv.reader")
 
-def ConvertTypes(element):
-    INT_FIELDS = ['COUNTYCODE', 'ED', 'LD', 'CD', 'SD', 'AD']
-    for field in INT_FIELDS:
+
+def vf_standardize_address(row, usps_key):
+    """Used for the NY State Voter File only."""
+    rhalfcode = '' if not row['RHALFCODE'] else row['RHALFCODE']
+    raddnumber = '' if not row['RADDNUMBER'] else row['RADDNUMBER']
+    rpredirection = '' if not row['RPREDIRECTION'] else row['RPREDIRECTION']
+    rstreetname = '' if not row['RSTREETNAME'] else row['RSTREETNAME']
+    rpostdirection = '' if not row['RPOSTDIRECTION'] else row['RPOSTDIRECTION']
+    rapartment = '' if not row['RAPARTMENT'] else row['RAPARTMENT']
+
+    # Build and verify full address, including apartment code
+    if ('APT' in str(row['RAPARTMENT']).upper()) \
+            or ('UNIT' in str(row['RAPARTMENT']).upper()) \
+            or (row['RAPARTMENT'] == ''):
+        address = "{} {} {} {} {} {}".format(
+            raddnumber,
+            rhalfcode,
+            rpredirection,
+            rstreetname,
+            rpostdirection,
+            rapartment)
+    else:
+        address = "{} {} {} {} {} APT {}".format(
+            raddnumber,
+            rhalfcode,
+            rpredirection,
+            rstreetname,
+            rpostdirection,
+            rapartment)
+    try:
+        address = address.upper()
+        addr = {'address': address, 'city': row['RCITY'], 'state': 'NY'}
+        result = address_information.verify(usps_key, addr)
+        zip4 = "-{}".format(result['zip4']) if result['zip4'] else ''
+        fmt_address = "{}, {} {} {}{}".format(
+            result['address'],
+            result['city'],
+            result['state'],
+            result['zip5'],
+            zip4)
+    except Exception:
+        fmt_address = None
+
+    # Build and verify street address (for geolocation)
+    street = "{} {} {} {} {}".format(
+        raddnumber,
+        rhalfcode,
+        rpredirection,
+        rstreetname,
+        rpostdirection)
+
+    try:
+        street = street.upper()
+        addr = {'address': street, 'city': row['RCITY'], 'state': 'NY'}
+        result = address_information.verify(usps_key, addr)
+        zip4 = "-{}".format(result['zip4']) if result['zip4'] else ''
+        fmt_street = "{}, {} {} {}{}".format(
+            result['address'],
+            result['city'],
+            result['state'],
+            result['zip5'],
+            zip4)
+    except Exception:
+        fmt_street = None
+
+    return fmt_address, fmt_street
+
+
+def build_formatted(element, usps_key):
+    """Generate row in formatted table."""
+
+    # Copy retained fields
+    RETAIN = {'LASTNAME': 'LastName',
+        'FIRSTNAME': 'FirstName',
+        'MIDDLENAME': 'MiddleName',
+        'NAMESUFFIX': 'NameSuffix',
+        'GENDER': 'Gender',
+        'ENROLLMENT': 'Enrollment',
+        'OTHERPARTY': 'OtherPartyEnrollment',
+        'ED': 'ElectionDistrict',
+        'LD': 'LegislativeDistrict',
+        'TOWNCITY': 'TownCityCode',
+        'WARD': 'Ward',
+        'CD': 'CongressionalDistrict',
+        'SD': 'SenateDistrict',
+        'AD': 'AssemblyDistrict',
+        'PREVNAME': 'PreviousName',
+        'COUNTYVRNUMBER': 'CountyVoterNumber',
+        'VRSOURCE': 'VoterRegistrationSource',
+        'IDREQUIRED': 'IDRequired',
+        'IDMET': 'IDMet',
+        'STATUS': 'Status',
+        'REASONCODE': 'StatusReasonCode',
+        'SBOEID': 'SBOEID',
+        'VoterHistory': 'VoterHistory'}
+    new = {RETAIN[k]: element[k] for k in RETAIN}
+
+    # Parse dates.  Must be returned as ISO string, not date object.
+    DATES = {'DOB': 'DateOfBirth',
+        'LASTVOTEDDATE': 'LastVotedDate',
+        'INACT_DATE': 'InactivatedDate',
+        'PURGE_DATE': 'PurgedDate'}
+    for k in DATES:
         try:
-            element[field] = int(element[field])
-        except ValueError:
-            element[field] = None
-    return element
+            dt = date(int(element[k][:4]),
+                int(element[k][4:6]),
+                int(element[k][6:8]))
+            new[DATES[k]] = str(dt)
+            if k == 'DOB':
+                new['Age'] = int((datetime.now().date() - dt).days / 365)
+        except Exception:
+            new[DATES[k]] = None
+
+    # Address fields
+    fmt_address, fmt_street = vf_standardize_address(element, usps_key)
+    new['Address'] = fmt_address
+    new['StreetAddress'] = fmt_street
+
+    return new
 
 def run(argv=None):
-  """Main entry point; defines and runs the pipeline."""
-  logging.info("Starting pipeline.")
+    """Main entry point; defines and runs the pipeline."""
+    logging.info("Starting pipeline.")
 
-  parser = argparse.ArgumentParser()
-  known_args, pipeline_args = parser.parse_known_args(argv)
-  pipeline_args.extend([
-    '--runner=DataflowRunner',
-    '--project=voterdb-test',
-    '--job_name=voter-pipeline'])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--usps_key',
+                        dest='usps_key',
+                        default=None,
+                        help='USPS API key')
+    known_args, pipeline_args = parser.parse_known_args(argv)
+    pipeline_args.extend([
+        '--project=voterdb-test',
+        '--job_name=voter-pipeline'])
 
-  pipeline_options = PipelineOptions(pipeline_args)
-  pipeline_options.view_as(SetupOptions).save_main_session = True
-  with beam.Pipeline(options=pipeline_options) as p:
+    if not known_args.usps_key:
+        raise Exception("Provide USPS API key.")
 
-     # TODO: Select rather than hard-code bucket/file name
-    raw = (p
-        | "beam.io.ReadFromText" >> beam.io.ReadFromText("gs://upload-raw/AllNYSVoters_2017-12-27.csv")
-        | "DictFromRawLine" >> beam.ParDo(DictFromRawLine()))
+    pipeline_options = PipelineOptions(pipeline_args)
+    pipeline_options.view_as(SetupOptions).save_main_session = True
+    with beam.Pipeline(options=pipeline_options) as p:
 
-    output = (raw
-        | "ConvertTypes" >> beam.Map(lambda x: ConvertTypes(x))
-        | "WriteRaw" >> beam.io.Write(
-            beam.io.BigQuerySink(table='VOTER.VoterFileRaw',
-                validate=True,
-                schema=RAW_VF_SCHEMA)))
+       # TODO: Select rather than hard-code bucket/file name
+        raw = (p
+               | "beam.io.ReadFromText" >> beam.io.ReadFromText("gs://upload-raw/AllNYSVoters_2017-12-27.csv")
+               | "DictFromRawLine" >> beam.ParDo(DictFromRawLine()))
 
-    output = (raw
-        | "FilterDict" >> beam.Map(lambda x: {'SBOEID': x['SBOEID'], 'STATUS': x['STATUS']})
-        | "WriteFiltered" >> beam.io.Write(
-            beam.io.BigQuerySink(table='VOTER.FilterTest',
-                validate=True,
-                schema='SBOEID:STRING,STATUS:STRING')))
+        output = (
+            raw | "WriteRaw" >> beam.io.Write(
+                beam.io.BigQuerySink(
+                    table='VOTER.VoterFileRaw',
+                    validate=True,
+                    schema=RAW_VF_SCHEMA,
+                    write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE,
+                    create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED)))
 
-    #output | beam.io.WriteToText('gs://output-stats/addresses.txt')
+        output = (raw
+            | "build_formatted" >> beam.Map(build_formatted, known_args.usps_key)
+            | "WriteFormatted" >> beam.io.Write(
+                beam.io.BigQuerySink(
+                    table='VOTER.Formatted',
+                    validate=True,
+                    schema=FORMATTED_SCHEMA,
+                    write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE,
+                    create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED)))
+
 
 if __name__ == '__main__':
-  logging.getLogger().setLevel(logging.INFO)
-  run()
+    logging.getLogger().setLevel(logging.INFO)
+    run()
